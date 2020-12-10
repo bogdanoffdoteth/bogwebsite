724eeb17e15f95e0ee401e013bfd879482b66e1a
bogdanoffdoteth <lesfreresbogdanoff123@gmail.com>
2020-12-10 12:33:01 +0800

    added Team profiles

--- src/Team.js
+++ src/Team.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
-import egUser from "./assets/eg-user.png";
+import bogdanoff_eth_dp from "./assets/Bogdanoff.eth_DP.png";
+import tormented_squidward_dp from "./assets/Tormented_Squidward_DP.png";
+import cream_eth_dp from "./assets/Cream.eth_DP.png";
 
 class TeamDiv extends Component{
   render(){
     return(
       <div className="col-md-4" align="center">
-        <img className="team-member-img" src={this.props.img} alt="Team Member Profile"></img>
+        <img className="team-member-img" src={this.props.img} alt={`${this.props.role} Profile`}></img>
         <h3>{this.props.name}</h3>
         <p>{this.props.role}</p>
       </div>
@@ -17,9 +19,9 @@ class Team extends Component{
 
   render(){
     const TeamList = [
-      {img:egUser ,name:"bogdanoff.eth", role:"founder/developer"},
-      {img:egUser,name:"john doe", role:"community manager"},
-      {img:egUser,name:"jane doe", role: "marketing manager"}
+      {img:bogdanoff_eth_dp ,name:"bogdanoff.eth", role:"founder/developer"},
+      {img:tormented_squidward_dp,name:"Tormented Squidward", role:"community manager"},
+      {img:cream_eth_dp,name:"Cream.eth", role: "marketing manager"}
     ];
     return(
       <div className="section">
